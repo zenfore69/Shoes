4d7fa1b0f4e1993793400e6390502369752edc36
zenfore1 <naxim2006@gmail.com>
2026-03-04 20:48:13 +0300

    Добавьте файлы проекта.

---
+++ Shoes/CurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoes
+{
+    class CurrentUser
+    {
+        public static string Role { get; set; } = "Гость";
+        public static string FullName { get; set; } = "";
+    }
+}
